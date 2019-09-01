2bb066389990e61dd8fb97efb2fe5941f4ced070
Aditya Phadte <adityaphadte13@gmail.com>
2019-09-01 23:11:04 +0530

    product and accounts apps created

--- ProductHunt/settings.py
+++ ProductHunt/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ProductHunt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'producthuntdb',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -118,3 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+try:
+    from .local_settings import *
+except:
+    pass
